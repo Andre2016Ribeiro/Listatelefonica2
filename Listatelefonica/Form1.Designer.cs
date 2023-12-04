f91aa0374afc677992cdf7dec6f7531bee5226dc
22it18 <22it18@gmail.com>
2023-12-04 11:27:56 +0000

    final

--- Listatelefonica/Form1.Designer.cs
+++ Listatelefonica/Form1.Designer.cs
@@ -28,10 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -39,9 +44,24 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Departamento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Extensão = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telemovel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apagar = new System.Windows.Forms.DataGridViewButtonColumn();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -65,7 +85,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(319, 156);
+            this.label3.Location = new System.Drawing.Point(296, 156);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 31);
             this.label3.TabIndex = 3;
@@ -74,6 +94,11 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.DarkSalmon;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.textBox1);
@@ -81,16 +106,48 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(55, 126);
+            this.groupBox1.Location = new System.Drawing.Point(0, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(756, 326);
+            this.groupBox1.Size = new System.Drawing.Size(762, 329);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Contacto";
             // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(457, 200);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(158, 33);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(454, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Telemovel";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(404, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(211, 33);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(401, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 31);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Departamento";
+            // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(322, 200);
+            this.textBox3.Location = new System.Drawing.Point(299, 200);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 33);
             this.textBox3.TabIndex = 7;
@@ -153,21 +210,117 @@
             this.comboBox2.TabIndex = 1;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(55, 118);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(766, 351);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(758, 325);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Contactatos";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(758, 325);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Favoritos";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Departamento,
+            this.Nome,
+            this.Telefone,
+            this.Extensão,
+            this.Telemovel,
+            this.Apagar});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(752, 319);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 41);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Adicionar aos favoritos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Departamento
+            // 
+            this.Departamento.HeaderText = "Departamento";
+            this.Departamento.Name = "Departamento";
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Telefone
+            // 
+            this.Telefone.HeaderText = "Telefone";
+            this.Telefone.Name = "Telefone";
+            // 
+            // Extensão
+            // 
+            this.Extensão.HeaderText = "Extensão";
+            this.Extensão.Name = "Extensão";
+            // 
+            // Telemovel
+            // 
+            this.Telemovel.HeaderText = "Telemovel";
+            this.Telemovel.Name = "Telemovel";
+            // 
+            // Apagar
+            // 
+            this.Apagar.HeaderText = "Apagar";
+            this.Apagar.Name = "Apagar";
+            this.Apagar.Text = "Sim";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(846, 500);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Lista telefonica";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -184,6 +337,21 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Departamento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Extensão;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telemovel;
+        private System.Windows.Forms.DataGridViewButtonColumn Apagar;
     }
 }
 
--- Listatelefonica/Form1.cs
+++ Listatelefonica/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -22,7 +23,29 @@ namespace Listatelefonica
 
         public void Form1_Load(object sender, EventArgs e)
         {
+            using (StreamReader sr = new StreamReader("favoritos.txt"))
+            {
+                string line;
 
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                    string[] tokensdata = line.Split(',');
+                    try
+                    {
+                        dataGridView1.Rows.Add(tokensdata[0], tokensdata[1], tokensdata[2], tokensdata[3], tokensdata[4]);
+
+                    }
+                    catch (Exception)
+                    {
+
+
+                    }
+
+
+
+                }
+            }
 
             foreach (var line in lineOfContents)
             {
@@ -30,31 +53,43 @@ namespace Listatelefonica
                 comboBox1.Items.Add(tokens[2] + "  -  " + tokens[3]);
             }
             foreach (var line in lineOfContentstelemovel)
-            { 
+            {
                 string[] tokenstelemovel = line.Split(',');
                 comboBox2.Items.Add(tokenstelemovel[2] + "  -  " + tokenstelemovel[3]);
 
             }
 
         }
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-           var s= comboBox1.SelectedIndex.ToString();
-            
+            var s = comboBox1.SelectedIndex.ToString();
+            var t = "";
             foreach (var line in lineOfContents)
             {
                 string[] tokens = line.Split(',');
-                if (tokens[0]==s)
+                if (tokens[0] == s)
                 {
-                    textBox1.Text= tokens[3];
+                    textBox1.Text = tokens[3];
                     textBox2.Text = tokens[1];
                     textBox3.Text = tokens[4];
-                }      
+                    textBox4.Text = tokens[2];
+                    if (tokens[5] != "") { t = tokens[5]; }
+                    else { t = "x"; }
+
+                }
+            }
+            foreach (var line in lineOfContentstelemovel)
+            {
+                string[] tokenstelemovel = line.Split(',');
+                if (tokenstelemovel[5] == t)
+                {
+                    textBox5.Text = tokenstelemovel[1];
+                    break;
+
+                }
+                else { textBox5.Text = ""; }
             }
 
         }
@@ -62,15 +97,106 @@ namespace Listatelefonica
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
             var s = comboBox2.SelectedIndex.ToString();
+            var t = "";
             foreach (var line in lineOfContentstelemovel)
             {
-                
+
                 string[] tokenstelemovel = line.Split(',');
                 if (tokenstelemovel[0] == s)
                 {
+                    textBox5.Text = tokenstelemovel[1];
                     textBox1.Text = tokenstelemovel[3];
-                    textBox2.Text = tokenstelemovel[1];
                     textBox3.Text = tokenstelemovel[4];
+                    textBox4.Text = tokenstelemovel[2];
+                    if (tokenstelemovel[5] != "") { t = tokenstelemovel[5]; }
+                    else { t = "x"; }
+                }
+            }
+            foreach (var line in lineOfContents)
+            {
+                string[] tokens = line.Split(',');
+                if (tokens[5] == t)
+                {
+                    textBox2.Text = tokens[1];
+                    textBox3.Text = tokens[4];
+                    break;
+
+                }
+                else { textBox2.Text = ""; }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextWriter tw = new StreamWriter("favoritos.txt", true);
+
+
+            tw.WriteLine(textBox1.Text.ToString() + "," + textBox4.Text.ToString() + "," + textBox2.Text.ToString() + "," + textBox3.Text.ToString() + "," + textBox5.Text.ToString());
+
+
+            tw.Close();
+            dataGridView1.Rows.Clear();
+            using (StreamReader sr = new StreamReader("favoritos.txt"))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] tokensdata = line.Split(',');
+                    try
+                    {
+                        dataGridView1.Rows.Add(tokensdata[0], tokensdata[1], tokensdata[2], tokensdata[3], tokensdata[4]);
+                    }
+                    catch (Exception)
+                    {
+
+
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                e.RowIndex >= 0)
+            {
+                TextWriter td = new StreamWriter("favoritos.txt");
+                td.WriteLine("");
+                td.Close();
+
+                for (int Row = 0; Row < dataGridView1.RowCount - 1; Row++)
+                {
+                    TextWriter tw = new StreamWriter("favoritos.txt", true);
+
+                    if (Row != e.RowIndex)
+                    {
+                        string a = dataGridView1.Rows[Row].Cells[0].Value.ToString() + "," + dataGridView1.Rows[Row].Cells[1].Value.ToString() + "," + dataGridView1.Rows[Row].Cells[2].Value.ToString() + "," + dataGridView1.Rows[Row].Cells[3].Value.ToString() + "," + dataGridView1.Rows[Row].Cells[4].Value.ToString();
+                        tw.WriteLine(a);
+                    }
+
+                    tw.Close();
+                }
+                dataGridView1.Rows.Clear();
+                using (StreamReader sr = new StreamReader("favoritos.txt"))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] tokensdata = line.Split(',');
+                        try
+                        {
+                            dataGridView1.Rows.Add(tokensdata[0], tokensdata[1], tokensdata[2], tokensdata[3], tokensdata[4]);
+                        }
+                        catch (Exception)
+                        {
+
+
+                        }
+                    }
                 }
             }
         }
